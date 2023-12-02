aa3f37db697f0484fdf1c9549bdbc35892e48931
ThanksCheersHrng <sistertree321@yahoo.com>
2023-12-02 21:42:32 +0000

    Trying again to push db_utils.py onto the online repo.

---
+++ db_utils.py
@@ -0,0 +1,9 @@
+class RDSDatabaseConnector: 
+    def __init__(self, param1, param2): 
+        self.param1 = param1
+        self.param2 = param2 
+        self.param3 = param2*param2
+        self.param4 = "cats, man, dude!"
+        
+    def method1(self, external_input): 
+        return #some func to do with param1+external_input 
