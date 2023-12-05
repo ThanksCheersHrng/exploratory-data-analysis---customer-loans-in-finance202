918f69f3a026f5b1ce04a598e51388eec3ee78f9
ThanksCheersHrng <sistertree321@yahoo.com>
2023-12-05 17:58:09 +0000

    Add dict param, engine method, and comments->class

--- db_utils.py
+++ db_utils.py
@@ -1,18 +1,32 @@
-class RDSDatabaseConnector: 
-    def __init__(self, param1, param2): 
-        self.param1 = param1
-        self.param2 = param2 
-        self.param3 = param2*param2
-        self.param4 = "cats, man, dude!"
-        
-    def method1(self, external_input): 
-        return #some func to do with param1+external_input 
-
-
 import yaml #this will allow us to read credentials.yaml as a dict
+from sqlalchemy.engine.url import URL 
 
-def load_credentials(file): #loads file and returns dictionary contained within
-	dictionary = yaml.load(open(file, 'r')
-	return(dictionary)
+#import pandas
+
+
+
+def load_credentials(file): #loads file and returns dictionary contained within #help from https://python.land/data-processing/python-yaml#Reading_and_parsing_a_YAML_file_with_Python
+	with open('config.yml', 'r') as file
+     new_dict = yaml.safe_load(file)
+    return new_dict
+
+
+class RDSDatabaseConnector: 
+    def __init__(self, dict_of_creds): 
+        self.dict_of_creds = dict_of_creds
+        
+    def method_engine(self): 
+        #initialise a SQLAlechemy engine from dict_of_creds #where TF did this come from? https://docs.sqlalchemy.org/en/20/core/engines.html #https://stackoverflow.com/questions/55895457/is-it-possible-to-pass-a-dictionary-into-create-engine-function-in-sqlalchemy
+        url = URL.create(**self.dict_of_creds)
+        engine = create_engine(url, echo=True)
+
+    def method_df(self): #Develop a method which extracts data from the RDS database and returns it as a Pandas DataFrame. The data is stored in a table called loan_payments. 
+         
+#Now create another function which saves the data to an appropriate file format to your local machine. This should speed up loading up the data when you're performing your EDA/analysis tasks. The function should save the data in .csv format, since we're dealing with tabular data.
+
+
+
+
+
 
 
