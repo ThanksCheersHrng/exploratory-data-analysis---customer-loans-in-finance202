e0a06579858f95c8bdfa46d5eba636a52460111d
ThanksCheersHrng <sistertree321@yahoo.com>
2023-12-07 08:38:27 +0000

    With the help of a much better coder, first functional draft of milestone 2: creating a class with functions that reads in credentials.

--- db_utils.py
+++ db_utils.py
@@ -1,32 +1,86 @@
-import yaml #this will allow us to read credentials.yaml as a dict
-from sqlalchemy.engine.url import URL 
-
-#import pandas
-
+import yaml
+from sqlalchemy import create_engine, text
+import pandas as pd
 
 
-def load_credentials(file): #loads file and returns dictionary contained within #help from https://python.land/data-processing/python-yaml#Reading_and_parsing_a_YAML_file_with_Python
-	with open('config.yml', 'r') as file
-     new_dict = yaml.safe_load(file)
+def load_credentials(file_path='credentials.yaml'):
+    """
+    Load a .yaml file into a dict
+
+    Args:
+        file_path: path to your yaml file
+
+    Returns:
+        new_dict: dict of database credentials
+
+    """
+    with open(file_path) as file:
+        new_dict = yaml.safe_load(file)
+
     return new_dict
 
 
-class RDSDatabaseConnector: 
+class RDSDatabaseConnector(): 
     def __init__(self, dict_of_creds): 
         self.dict_of_creds = dict_of_creds
+
+    def init_sql_engine(self):
+        """
+        Initialize the SQLAlchemy engine
+
+        """
+        engine = create_engine('postgresql://{username}:{password}@{endpoint}:{port}/{database}'.format(username=self.dict_of_creds['RDS_USER'],
+                                                                                                    password=self.dict_of_creds['RDS_PASSWORD'],
+                                                                                                    endpoint=self.dict_of_creds['RDS_HOST'],
+                                                                                                    port=self.dict_of_creds['RDS_PORT'],
+                                                                                                    database=self.dict_of_creds['RDS_DATABASE']
+                                                                                                    ))
+
+        return engine
+    
+    def extract_data_from_db(self):
+        """
+        Extract the data from the database into a dataframe
+
+        Returns:
+            df: pandas dataframe of data from the database
+
+        """
+        sql_engine = self.init_sql_engine()
         
-    def method_engine(self): 
-        #initialise a SQLAlechemy engine from dict_of_creds #where TF did this come from? https://docs.sqlalchemy.org/en/20/core/engines.html #https://stackoverflow.com/questions/55895457/is-it-possible-to-pass-a-dictionary-into-create-engine-function-in-sqlalchemy
-        url = URL.create(**self.dict_of_creds)
-        engine = create_engine(url, echo=True)
+        table_name = 'loan_payments'
 
-    def method_df(self): #Develop a method which extracts data from the RDS database and returns it as a Pandas DataFrame. The data is stored in a table called loan_payments. 
-         
-#Now create another function which saves the data to an appropriate file format to your local machine. This should speed up loading up the data when you're performing your EDA/analysis tasks. The function should save the data in .csv format, since we're dealing with tabular data.
+        sql = 'select * from {table_name};'.format(table_name=table_name)
+
+        df = pd.DataFrame(sql_engine.connect().execute(text(sql)))
+
+        return df
+
+    def export_dataframe_to_csv(self, path='', file_name='dataframe.csv'):
+        """
+        Export a dataframe into a .csv file
+
+        Args:
+            path: the folder location to create the .csv file in e.g. path/to/your/folder/
+                    empty string is current working dir
+            file_name: the name of the .csv file to export to
+        """
+        df = self.extract_data_from_db()
+
+        full_path = path + file_name
+
+        df.to_csv(full_path, encoding='utf-8')
 
 
+"""
+Checking it all runs...
+"""
 
+creds = load_credentials()
 
+rds_connector = RDSDatabaseConnector(creds)
+
+rds_connector.export_dataframe_to_csv()
 
 
 
