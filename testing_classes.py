e2dd67cc895a1ca3a9bd197234b73c5721c9c704
ThanksCheersHrng <sistertree321@yahoo.com>
2024-01-21 18:26:54 +0000

    changed name error to attribute error

--- testing_classes.py
+++ testing_classes.py
@@ -1,7 +1,21 @@
 #This is a space where I can test out the methods of my classes. 
+import numpy as np
+import pandas as pd
 
-import data_cleaning_for_EDA
+#import data_cleaning_for_EDA
 import load_to_pandas
+
+#temporary direct copy/paste instead of import- why is name 'DataFrameInfo' not defined??? 
+#Now I get this error instead: AttributeError: 'DataFrameInfo' object has no attribute 'dtypes' 
+
+class DataFrameInfo():
+    def __init__(self, data_frame):
+        self.data_frame = data_frame
+    def data_types(self): 
+        data_types = self.dtypes
+        print(data_types)
+    def col_names(self): 
+        print(self.columns)
 
 #Giving my data frame a short and sweet name to work with 
 df = load_to_pandas.finance_df
