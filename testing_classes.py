ade7cd52655f2631f692adedea917ec402c7c010
ThanksCheersHrng <sistertree321@yahoo.com>
2024-02-01 20:02:07 +0000

    added more methods to investigate the 98% rate of NA's

--- testing_classes.py
+++ testing_classes.py
@@ -12,10 +12,13 @@ finance_df['employment_length'] = finance_df['employment_length'].str.extract("(
 finance_df['term'] = finance_df['term'].str.extract("([-+]?\d*\.\d+|[-+]?\d+)").astype(float)
 finance_df.rename(columns = {'employment_length':'years_of_employment', 'term' : 'term_length_in_months'}, inplace = True) 
 
-
 class DataFrameInfo():
     def __init__(self, data_frame):
         self.data_frame = data_frame
+
+    def count_elements(self):
+        element_count = self.data_frame.size
+        print(element_count)
 
     def data_types(self): 
         data_types = self.data_frame.dtypes
@@ -24,7 +27,7 @@ class DataFrameInfo():
     def col_names(self): 
         header = self.data_frame.columns 
     
-    def stats(self) -> None: 
+    def stats(self): 
         means = self.data_frame.mean(numeric_only=True) #addition unsupported for float and str, so force float and ignore str?  
         meds = self.data_frame.median(numeric_only=True)
         mode = self.data_frame.mode(axis=1, dropna=True)
@@ -39,30 +42,32 @@ class DataFrameInfo():
         # table = [[columns, means, medians, modes], [self.col_names, means, meds, mode]]
         # print(tabulate(table))
 
+    def tabulate_non_null_values(self): 
+        print(self.data_frame.value_counts())
 
-    def count_null(self) -> None: 
+    def count_null(self): 
         # approach number 1
         # createa a count of this somehow: 
             # (self.data_frame.isna)  # this is boolean same-sized df showing places of NA elts (so like, count if =True)
         # approach number 2
-        all_count = self.data_frame.size # n. elts in array 
-        non_null_count = self.data_frame.count() #counts non-null elts?  
+        all_count = self.data_frame.size # n. elts in array
+        non_null_count = self.data_frame.count() # n. non-null elts
         null_count = all_count - non_null_count
         print(null_count)
     
-    def perc_null(self) -> None: 
+    def perc_null(self): 
         numerator = self.data_frame.size - self.data_frame.count()
         denominator = self.data_frame.size
         print(100*numerator/denominator)
     
-    def print_shape(self) -> None: 
+    def print_shape(self): 
         print(self.data_frame.shape)
     
-    def count_distinct(self) -> None:
+    def count_distinct(self):
         # print(self.data_frame.) 
         pass
     
-    def cor_coef(self) -> None: 
+    def cor_coef(self): 
         pass 
 
 df = DataFrameInfo(finance_df)
@@ -71,9 +76,11 @@ df = DataFrameInfo(finance_df)
 # df.col_names()
 # df.stats()
 # df.print_shape() # (54231, 44)
-# df.count_null() #that works. They're all in the 2 milliion's, so roughly half NA's. 
-# df.perc_null() # they all come out to around 98% which is bonkers because how is 2/5 close to 98%? 
+# df.count_null() #that works. They're all in the 2 milliion's, but different values 
+# df.perc_null() # they all come out to around 98%, which is bonkers, but fits with the null count. 
+# df.count_elements() # 2386164
 
+df.tabulate_non_null_values()
 
 """ 
 #Giving my data frame a short and sweet name to work with 
