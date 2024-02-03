da0f43344376af8fb2fb64a2445a9fbe1eedb5b4
ThanksCheersHrng <sistertree321@yahoo.com>
2024-02-03 14:14:04 +0000

    improved date parsing according to FutureWarnings and improved initial data read-ing cleaning according to Syntax Warning. Not sure why df.data_types isn't printing, though it's set up to do so in data_cleaning_for_EDA.

--- testing_classes.py
+++ testing_classes.py
@@ -5,12 +5,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import imputing_methods as im
 
+# I keep getting date parsing warnings when I least expect them so I'm going to try specifying a single format now. 
+# Apart from the columns I turn into floats below, all the other date-type columns in finance_df appear to be Month-Year in shape:
+def parse_to_datetime_mo_yr(date_str):
+    return pd.to_datetime(date_str, format='%m-%Y', errors='coerce')
 
-#look into parsing dates more effectively with a custom date parser? #see notes in date_parser.txt
-finance_df = pd.read_csv("dataframe.csv", parse_dates= ['issue_date', 'earliest_credit_line','last_payment_date', 'next_payment_date','last_credit_pull_date'])
-finance_df['employment_length'] = finance_df['employment_length'].str.extract("([-+]?\d*\.\d+|[-+]?\d+)").astype(float)
+finance_df = pd.read_csv(
+    "dataframe.csv",
+    parse_dates=['issue_date', 'earliest_credit_line', 'last_payment_date', 'next_payment_date', 'last_credit_pull_date'],
+    date_format='%m-%Y',
+    converters={'issue_date': parse_to_datetime_mo_yr, 
+                'earliest_credit_line': parse_to_datetime_mo_yr,
+                'last_payment_date': parse_to_datetime_mo_yr,
+                'next_payment_date': parse_to_datetime_mo_yr,
+                'last_credit_pull_date': parse_to_datetime_mo_yr}
+)
+#finance_df = pd.read_csv("dataframe.csv", parse_dates= ['issue_date', 'earliest_credit_line','last_payment_date', 'next_payment_date','last_credit_pull_date'], date_parser=parse_to_datetime_mo_yr)
+finance_df['employment_length'] = finance_df['employment_length'].str.extract(r"([-+]?\d*\.\d+|[-+]?\d+)").astype(float)
 # print(finance_df['term'].head())
-finance_df['term'] = finance_df['term'].str.extract("([-+]?\d*\.\d+|[-+]?\d+)").astype(float)
+finance_df['term'] = finance_df['term'].str.extract(r"([-+]?\d*\.\d+|[-+]?\d+)").astype(float)
 finance_df.rename(columns = {'employment_length':'years_of_employment', 'term' : 'term_length_in_months'}, inplace = True) 
 
 class MultipleInheritanceTestClass(im.Plotter, dc.DataFrameInfo):
@@ -18,4 +31,10 @@ class MultipleInheritanceTestClass(im.Plotter, dc.DataFrameInfo):
 
 df = MultipleInheritanceTestClass(finance_df)
 
-df.plot_column('issue_date')
+# df.plot_column('issue_date') # Late 2009 looks very popular. 
+
+# df.col_names()
+
+# df.plot_column('home_ownership') # unsupported data type
+
+df.data_types #why isn't this actually printing??? 
